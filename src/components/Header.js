efb9f97a2595fa6828f5dc98aa523e8e020e75ac
Carmen V Ramos <carmenvramos@yahoo.com>
2018-08-17 08:46:16 -0700

    Header and Home Initial Setup

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Search from './search/Search';
+import { Link, Route } from 'react-router-dom';
+// import style from './Header.css';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/cards">Search</Link>
+            </li> 
+          </ul>
+        </nav>  
+        <section>
+          <Route component={Search}/>
+        </section>    
+      </div>
+    );
+
+  }
+    
+}
+export default Header;
