b728076e0c0e3364070440012d5de1cfc541e967
Robyn Navarro <rnavarro182@gmail.com>
2018-08-18 10:09:59 -0700

    favorites are being added

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addFavorite } from '../../services/favoritesApi.js';
 
 export default class Card extends Component {
 
   static propTypes = {
     card: PropTypes.object.isRequired
+  };
+
+  handleClick = () => {
+    addFavorite(this.props.card)
+      .catch(console.log);
   };
 
   render() {
@@ -14,6 +20,7 @@ export default class Card extends Component {
     return (
       <li>
         <img src={imageUrl} alt={`${name}(${setName})`}/>
+        <button onClick={this.handleClick}>⭐️</button>
       </li>
     );
   }
--- src/services/favoritesApi.js
+++ src/services/favoritesApi.js
@@ -3,7 +3,7 @@ import { put, get, del } from './request';
 const URL = 'https://mysticaltutor-e1723.firebaseio.com';
 const FAVORITES_URL = `${URL}/favorites`;
 
-const getFavoriteUrl = id => `S{FAVORITE_URL}/id-${id}.json`;
+const getFavoriteUrl = id => `${FAVORITES_URL}/id-${id}.json`;
 
 export const addFavorite = ({ id, name, imageUrl }) => {
   const url = getFavoriteUrl(id);
