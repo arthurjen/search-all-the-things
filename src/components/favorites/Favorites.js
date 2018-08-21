e42825a884c70715944a771f38b9a0ca3cacde12
Arthur Jen <arthurljen@gmail.com>
2018-08-21 15:00:39 -0700

    changed Favorite to update realtime

--- src/components/favorites/Favorites.js
+++ src/components/favorites/Favorites.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import { getFavorites } from '../../services/favoritesApi';
 import Card from '../cards/Card';
 import style from './Favorites.css';
-class Favorites extends Component {
+require('dotenv').config();
+const API_KEY = process.env.API_KEY;
+import firebase from 'firebase';
+const config = {
+  apiKey: API_KEY,
+  authDomain: 'mysticaltutor-e1723.firebaseapp.com',
+  databaseURL: 'https://mysticaltutor-e1723.firebaseio.com',
+  projectId: 'mysticaltutor-e1723',
+  storageBucket: 'mysticaltutor-e1723.appspot.com',
+  messagingSenderId: '42370156112'
+};
+firebase.initializeApp(config);
+const database = firebase.database();
 
+class Favorites extends Component {
+  
   state = {
     favorites: null
   };
 
+  
+
   componentDidMount() {
-    getFavorites()
-      .then(favorites => {
-        this.setState({ favorites });
-      })
-      .catch(console.log);
+
+    const favoritesRef = database.ref('favorites');
+    
+    favoritesRef.on('value', (snapshot) => {
+      const response = snapshot.val();
+      const favorites = Object.keys(response)
+        .map(key => response[key]);
+      this.setState({ favorites });
+    });
   }
 
   render() {
